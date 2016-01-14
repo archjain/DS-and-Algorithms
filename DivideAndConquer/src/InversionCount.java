4fac34e39f109fadaa159af91a08b16f97da91ab
archjain <jain.archit04@yahoo.com>
2016-01-14 14:46:05 -0700

    initial commit

---
+++ DivideAndConquer/src/InversionCount.java
@@ -0,0 +1,74 @@
+public class InversionCount {
+
+	public int count(int[] A) {
+		if (A.length <= 1) {
+			return 0;
+		} else {
+			int[] f = new int[A.length / 2];
+			int[] s = new int[A.length - A.length / 2];
+			for (int i = 0; i < f.length; i++) {
+				f[i] = A[i];
+			}
+			int k = f.length;
+			for (int j = 0; j < s.length; j++) {
+				s[j] = A[k];
+				k++;
+			}
+			count(f);
+			count(s);
+			int Z = splitInv(f, s, A);
+			return Z;
+		}
+	}
+
+	int c = 0;
+
+	public int splitInv(int[] f, int[] s, int[] A) {
+
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		while (i < f.length && j < s.length) {
+			if (f[i] <= s[j]) {
+				A[k] = f[i];
+				i++;
+				k++;
+
+			} else {
+				A[k] = s[j];
+				j++;
+				k++;
+				c = c + f.length - i;
+
+			}
+		}
+		if (i < f.length) {
+			while (i < f.length) {
+				A[k] = f[i];
+				i++;
+				k++;
+
+			}
+		}
+		if (j < s.length) {
+			while (j < s.length) {
+				A[k] = s[j];
+				j++;
+				k++;
+			}
+		}
+		System.out.println(c);
+		return c;
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] A = { 6, 5, 4, 3, 2, 1 };
+		InversionCount inv = new InversionCount();
+
+		System.out.println(inv.count(A));
+	}
+
+}
---
+++ DivideAndConquer/src/MergeSort.java
@@ -0,0 +1,72 @@
+import java.util.Arrays;
+
+public class MergeSort {
+
+	public int[] sort(int[] A) {
+		if (A.length <= 1) {
+			return A;
+		}
+		int[] f = new int[A.length / 2];
+		int[] s = new int[A.length - A.length / 2];
+		for (int i = 0; i < f.length; i++) {
+			f[i] = A[i];
+		}
+		int k = 0;
+		for (int j = 0; j < s.length; j++) {
+			s[j] = A[f.length + k];
+			k++;
+		}
+		sort(f);
+		sort(s);
+		int[] O = merge(f, s, A);
+		return O;
+	}
+
+	public int[] merge(int[] f, int[] s, int[] A) {
+		int[] O = new int[A.length];
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		while (i < f.length && j < s.length) {
+			if (f[i] > s[j]) {
+				A[k] = s[j];
+				j++;
+
+			} else {
+				A[k] = f[i];
+				i++;
+			}
+			k++;
+		}
+		if (i < f.length) {
+			while (i < f.length) {
+				A[k] = f[i];
+				k++;
+				i++;
+			}
+		} else {
+			while (j < s.length) {
+				A[k] = s[j];
+				k++;
+				j++;
+			}
+		}
+		for (int p = 0; p < A.length; p++)
+			System.out.println(p + "::" + A[p]);
+		return A;
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] A = { 1, 2, 3, 5, 2, 4, 6 };
+		MergeSort m = new MergeSort();
+
+		int[] X = m.sort(A);
+		for (int i = 0; i < X.length; i++) {
+			System.out.println(X[i]);
+		}
+
+	}
+
+}
