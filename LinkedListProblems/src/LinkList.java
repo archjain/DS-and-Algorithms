f44f5a351cf7e251087734a2429a3bff34f7cb65
archjain <jain.archit04@yahoo.com>
2016-01-14 20:41:44 -0700

    LinkList-Find A cycle problem

---
+++ LinkedListProblems/src/LinkList.java
@@ -0,0 +1,36 @@
+
+public class LinkList {
+     Node head=null;
+     Node tail=null;
+	public void addNodeAtLast(Node newNode){
+		if(head==null)
+		{
+			head=newNode;
+			tail=newNode;
+			newNode.next=null;
+		}
+		else
+		{
+			tail.next=newNode;
+			tail=newNode;
+			tail.next=null;
+			
+		}
+		
+	}
+	
+	public void insertLoop(int loopIndex)
+	{
+		int i = 0;
+		Node temp=head;
+		while(i!=loopIndex)
+		{
+			temp=temp.next;
+			i++;
+		}
+		tail.next=temp;
+	}
+	
+	
+	
+}
---
+++ LinkedListProblems/src/Node.java
@@ -0,0 +1,11 @@
+
+public class Node {
+
+	private int data;
+	Node next;
+    Node(int data)
+    {
+    	this.data =  data;
+    }
+   
+}
